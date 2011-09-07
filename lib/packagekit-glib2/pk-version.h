31436710549bea6068dc4a35c5500da99b1ac02a
Matthias Klumpp <matthias@tenstral.net>
2011-09-07 19:12:35 +0200

    Imported Upstream version 0.6.18

--- lib/packagekit-glib2/pk-version.h
+++ lib/packagekit-glib2/pk-version.h
@@ -30,7 +30,7 @@
  */
 #define PK_MAJOR_VERSION				(0)
 #define PK_MINOR_VERSION				(6)
-#define PK_MICRO_VERSION				(17)
+#define PK_MICRO_VERSION				(18)
 
 /* check whether a PackageKit version equal to or greater than
  * major.minor.micro.
