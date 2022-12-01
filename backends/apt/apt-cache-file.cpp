977c636966bda4a4a43110213cf9dc9cc8c30488
Matthias Klumpp <mak@debian.org>
2022-12-01 20:26:17 +0100

    New upstream version 1.2.6

--- backends/apt/apt-cache-file.cpp
+++ backends/apt/apt-cache-file.cpp
@@ -285,8 +285,8 @@ bool AptCacheFile::doAutomaticRemove()
 
     // Now see if we destroyed anything
     if ((*this)->BrokenCount() != 0) {
-        cout << "Hmm, seems like the AutoRemover destroyed something which really\n"
-                "shouldn't happen. Please file a bug report against apt." << endl;
+        g_warning("Seems like the AutoRemover destroyed something which really "
+                  "shouldn't happen. Please file a bug report against APT.");
         // TODO call show_broken
         //       ShowBroken(c1out,cache,false);
         return _error->Error("Internal Error, AutoRemover broke stuff");
@@ -380,13 +380,11 @@ PkgInfo AptCacheFile::resolvePkgID(const gchar *packageId)
             strcmp(ver.VerStr(), parts[PK_PACKAGE_ID_VERSION]) == 0)
         return PkgInfo(ver, piAction);
 
-    const pkgCache::VerIterator &candidateVer = findCandidateVer(pkg);
     // check to see if the provided package isn't virtual too
-    // also iterate through all the available versions
-    for (auto candidate = candidateVer; !candidate.end(); candidate++) {
-        if (strcmp(candidate.VerStr(), parts[PK_PACKAGE_ID_VERSION]) == 0) {
-            return PkgInfo(ver, piAction);
-        }
+    // also iterate through all available past versions
+    for (auto candidateVer = findCandidateVer(pkg); !candidateVer.end(); candidateVer++) {
+        if (strcmp(candidateVer.VerStr(), parts[PK_PACKAGE_ID_VERSION]) == 0)
+            return PkgInfo(candidateVer, piAction);
     }
 
     return PkgInfo(ver, piAction);
